6076552b611bad11e6151d32309a3c25864fb36c
“Ejima-we” <takumi941503@gmail.com>
2021-06-27 14:30:28 +0000

    favorite機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_130602) do
+ActiveRecord::Schema.define(version: 2021_06_27_135333) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_06_27_130602) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
+    t.integer "user_id"
+    t.integer "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "blog_id"
     t.datetime "created_at", null: false
